7ab5a958c48d2cfea20d4605adc630d86adb29f5
Gabriel Szucs <g.szucs@live.com>
2025-04-06 15:55:42 -0300

    learn(ap07-react) encerra a seção 2.2 da apostila

---
+++ exibe-fotos/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Exibe Fotos</h1>
+      <i className='pi pi-search'></i>
+    </div>
+  )
+}
+
+export default App
