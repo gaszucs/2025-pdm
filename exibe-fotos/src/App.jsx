658c72ad92f1af4434f8137f6e43031c94d12b79
Gabriel Szucs <g.szucs@live.com>
2025-04-06 18:32:00 -0300

    Learn(ap07-react): lida com a pagina 20 construimos o pexelsClient usando react-dotenv para abrigar a chave o proximo passo e fazer uma requisição em busca de fotos

--- exibe-fotos/src/App.jsx
+++ exibe-fotos/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import Busca from './Components/Busca'
+import env from 'react-dotenv'
+import { createClient } from 'pexels'
 
 class App  extends React.Component {
+  pexelsClient = null
+
+  componentDidMount () {
+    this.pexelsClient = createClient(env.PEXELS_KEY)
+  }
 
   onBuscaRealizada = (termo) => {
     console.log(termo)
   }
   render () {
+    console.log(env.PEXELS_KEY)
     return (
       <div className='grid justify-content-center align-items-center'>
         <div className='col-12 '>
@@ -16,6 +25,7 @@ class App  extends React.Component {
         <div className='col-12 '>
   
           <Busca dica='buscar algo... ' />
+          onBuscaRealizada={this.onBuscaRealizada}
         </div>
   
       </div>
