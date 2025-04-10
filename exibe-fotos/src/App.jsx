98d563b70d659b71a68937caa4b2b7b0a6bcede1
Gabriel Szucs <g.szucs@live.com>
2025-04-09 22:53:20 -0300

    learn(ap07-react): encerra a seção 2.13 ja fizemos uso da axios falta tornar a exibição de fotos responsiva tambem vamos implementar um back-end a fim de isolar a chave de API

--- exibe-fotos/src/App.jsx
+++ exibe-fotos/src/App.jsx
@@ -1,31 +1,64 @@
 import React from 'react'
 import Busca from './Components/Busca'
 import env from 'react-dotenv'
-import { createClient } from 'pexels'
+//import { createClient } from 'pexels'
+import PexelsClient from './utils/PexelsClient'
+import ListaImagens from './Components/ListaImagens'
+import PexelsLogo from './Components/PexelsLogo'
 
 class App  extends React.Component {
   pexelsClient = null
 
-  componentDidMount () {
-    this.pexelsClient = createClient(env.PEXELS_KEY)
+  state = {
+    photos: []
   }
 
-  onBuscaRealizada = (termo) => {
-    console.log(termo)
+  componentDidMount () {
+    // this.pexelsClient = createClient(env.PEXELS_KEY)
   }
+  onBuscaRealizada = (termo) => {
+    this.pexelsClient.get('/search', {
+      params: {
+        query: termo,
+        per_page: 10
+      }
+    })
+      .then((result) =>
+        this.setState({ photos: result.data.photos })
+      )
+      .catch((error) => {
+        console.error('Error fetching photos:', error)
+      })
+  }
+
+  // onBuscaRealizada = (termo) => {
+  //   this.pexelsClient.photos.search({ query: termo, per_page: 10 })
+  //     .then((result) =>
+  //       this.setState({ photos: result.photos })
+  //     )
+  //     .catch((error) => {
+  //       console.error('Error fetching photos:', error)
+  //     })
+  //   }
   render () {
     console.log(env.PEXELS_KEY)
     return (
       <div className='grid justify-content-center align-items-center'>
+        <div className='col-12'>
+          <PexelsLogo/>
+        </div>
         <div className='col-12 '>
   
-          <h1>Exibe uma lista de...</h1>
+          <h1 className='text-center'>Exibe uma lista de...</h1>
   
         </div>
         <div className='col-12 '>
   
           <Busca dica='buscar algo... ' />
           onBuscaRealizada={this.onBuscaRealizada}
+        </div>
+        <div className='col-12'>
+          <ListaImagens photos={this.state.photos} />
         </div>
   
       </div>
